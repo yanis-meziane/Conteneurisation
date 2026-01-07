18cc78f5a1e49ab9c5061ecb332c175067364c64
Yanis Meziane <yanismeziane04@icloud.com>
2026-01-07 16:16:03 +0100

    Réalisation TP2

--- ingnum/RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
+++ ingnum/RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
@@ -1,13 +1,20 @@
 package com.ingnum.rentalservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class BonjourController {
 
+    @Value("${php.service.url}")
+    private String phpServiceUrl;
+
     @GetMapping("/bonjour")
     public String bonjour() {
-        return "bonjour";
+        RestTemplate restTemplate = new RestTemplate();
+        String prenom = restTemplate.getForObject(phpServiceUrl, String.class);
+        return "Bonjour " + prenom;
     }
 }
