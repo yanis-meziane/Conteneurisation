5ba87a6176bed22c3232ce8967cb25d2cba9e39a
Yanis Meziane <yanismeziane04@icloud.com>
2026-01-07 14:57:31 +0100

    Ajout TP1

---
+++ ingnum/RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
@@ -0,0 +1,13 @@
+package com.ingnum.rentalservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BonjourController {
+
+    @GetMapping("/bonjour")
+    public String bonjour() {
+        return "bonjour";
+    }
+}
